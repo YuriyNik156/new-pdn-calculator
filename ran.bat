4d35fdd6fde291329ccd3f8cd32951d966da3ecd
YuriyNik156 <yuriy.nik156@gmail.com>
2025-11-06 10:33:23 +0300

    Initial commit: PDN Calculator project ready for deploy

--- ran.bat
+++ ran.bat
@@ -2,3 +2,11 @@
 call .\venv\Scripts\activate
 uvicorn main:app --reload --port 8001
 pause
+
+
+git init
+git add .
+git commit -m "Initial commit: PDN Calculator project ready for deploy"
+git branch -M main
+git remote add origin https://github.com/YuriyNik156/pdn-calculator-new.git
+git push -u origin main
